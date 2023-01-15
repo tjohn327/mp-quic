beb321dace2fdf2f86d7ea3f147c110c04c6c89f
Abdoueck632 <abdouseck632@gmail.com>
2023-01-16 00:25:50 +0100

    modify forwardSecureAEAD in relay configure

--- internal/handshake/crypto_setup_server.go
+++ internal/handshake/crypto_setup_server.go
@@ -476,6 +476,9 @@ func (h *cryptoSetupServer) SetDerivationKey(otherKey []byte, myKey []byte, othe
 	utils.Infof("--> nullAEAD %+v", h.keyExchange)
 
 	// I try to cheat to have this type of function. So it's a bit of a do-it-yourself
+	h.forwardSecureAEAD, _ = crypto.NewAEADAESGCM12(myKey, otherKey, myIV, otherIV)
+
+	// I try to cheat to have this type of function. So it's a bit of a do-it-yourself
 	h.keyDerivation = func(forwardSecure bool, sharedSecret, nonces []byte, connID protocol.ConnectionID, chlo []byte, scfg []byte, cert []byte, divNonce []byte, pers protocol.Perspective) (
 		crypto.AEAD,
 		error,
